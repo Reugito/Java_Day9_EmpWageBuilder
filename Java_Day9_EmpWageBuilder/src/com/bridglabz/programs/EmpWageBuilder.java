3e15908f115748f0f2508b45206873f628547a98
Reugito <raosahebdhotreedu@gmail.com>
2021-07-01 13:48:22 +0530

    adding employee attendance

--- Java_Day9_EmpWageBuilder/src/com/bridglabz/programs/EmpWageBuilder.java
+++ Java_Day9_EmpWageBuilder/src/com/bridglabz/programs/EmpWageBuilder.java
@@ -1,5 +1,12 @@
 package com.bridglabz.programs;
 
+import java.util.Random;
+
 public class EmpWageBuilder {
 
+	public static int getAttendance() {
+		Random random = new Random();
+		int check = random.nextInt(3);
+		return check;
+	}
 }
--- Java_Day9_EmpWageBuilder/src/com/bridglabz/programs/EmpWageBuilderMain.java
+++ Java_Day9_EmpWageBuilder/src/com/bridglabz/programs/EmpWageBuilderMain.java
@@ -3,7 +3,8 @@ package com.bridglabz.programs;
 public class EmpWageBuilderMain {
 
 	public static void main(String[] args) {
-		
+		EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+		empWageBuilder.getAttendance();
 	}
 
 }
