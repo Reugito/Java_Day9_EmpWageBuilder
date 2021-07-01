6398ad3c6531551d646a6d80bb8997ae7495420d
Reugito <raosahebdhotreedu@gmail.com>
2021-07-01 14:12:16 +0530

    employee wager using switch case

--- Java_Day9_EmpWageBuilder/src/com/bridglabz/programs/EmpWageBuilder.java
+++ Java_Day9_EmpWageBuilder/src/com/bridglabz/programs/EmpWageBuilder.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class EmpWageBuilder {
 	public static final int WagePerHr = 20, FullDayHr = 8, PartThimeHr = 4;
 	public static final int isFulltime = 1, isParttime = 2;
+	public static int day = 0;
 	
 	public static int getAttendance() {
 		Random random = new Random();
@@ -21,6 +22,23 @@ public class EmpWageBuilder {
     	int partTimeWage = WagePerHr * PartThimeHr; 
         return partTimeWage;
     }
+	
+	public static int usingSwitch() {
+		int check = getAttendance() ;
+		day++;
+		switch (check) {
+			case isFulltime: 
+				System.out.println("Day"+day+" = "+getDailyWager());
+				return getDailyWager();
+		case isParttime:  
+				System.out.println("Day"+day+" = "+getPartTimeWage());
+				return getPartTimeWage();
+			default:
+				System.out.println("Day"+day+" = 0");
+			
+		return 0;
+		}
+	}
 }
 
 
